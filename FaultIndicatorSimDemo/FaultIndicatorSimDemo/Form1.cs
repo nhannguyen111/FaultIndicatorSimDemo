f4f28955ca3f0e1965ff5df41260606bc51d3b07
Nhan Nguyen <115508271+nhannguyen111@users.noreply.github.com>
2024-12-06 10:00:11 -0800

    Circle No Fault Indicator
    Added circles that indicates no fault state

--- FaultIndicatorSimDemo/FaultIndicatorSimDemo/Form1.cs
+++ FaultIndicatorSimDemo/FaultIndicatorSimDemo/Form1.cs
@@ -6,5 +6,57 @@ namespace FaultIndicatorSimDemo
         {
             InitializeComponent();
         }
+
+        private void panel15_Paint(object sender, PaintEventArgs e)
+        {
+            // Get the Graphics object
+            Graphics g = e.Graphics;
+
+            // Define the pen to draw the circle
+            using (Pen pen = new Pen(Color.White, 2)) // Blue color, 2px thickness
+            {
+                // Draw a circle
+                g.DrawEllipse(pen, 0, 0, 10, 10); // x, y, width, height
+            }
+        }
+
+        private void panel16_Paint(object sender, PaintEventArgs e)
+        {
+            // Get the Graphics object
+            Graphics g = e.Graphics;
+
+            // Define the pen to draw the circle
+            using (Pen pen = new Pen(Color.White, 2)) // Blue color, 2px thickness
+            {
+                // Draw a circle
+                g.DrawEllipse(pen, 0, 0, 10, 10); // x, y, width, height
+            }
+        }
+
+        private void panel17_Paint(object sender, PaintEventArgs e)
+        {
+            // Get the Graphics object
+            Graphics g = e.Graphics;
+
+            // Define the pen to draw the circle
+            using (Pen pen = new Pen(Color.White, 2)) // Blue color, 2px thickness
+            {
+                // Draw a circle
+                g.DrawEllipse(pen, 0, 0, 10, 10); // x, y, width, height
+            }
+        }
+
+        private void panel18_Paint(object sender, PaintEventArgs e)
+        {
+            // Get the Graphics object
+            Graphics g = e.Graphics;
+
+            // Define the pen to draw the circle
+            using (Pen pen = new Pen(Color.White, 2)) // Blue color, 2px thickness
+            {
+                // Draw a circle
+                g.DrawEllipse(pen, 0, 0, 10, 10); // x, y, width, height
+            }
+        }
     }
 }
